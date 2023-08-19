cddac3ce6ce9e8369fa8ed49cddeda3f4326085b
wdwf <w_dwf@hotmail.com>
2023-08-19 15:39:23 -0300

    :construction: chore: create page login

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,7 +1,74 @@
+import { ButtonComposition as Button } from "../../components/Button";
+import logo from "../../assets/imgs/logo.png";
+import imageCompany from "../../assets/imgs/imgInit.png";
+import { Link } from "react-router-dom";
+import { ArrowRight } from "@phosphor-icons/react";
+import { Input } from "../../components/Input";
 import * as S from "./styles";
+import { Form, useForm } from "../../components/Form";
+import { z } from "zod";
+import { LoginProps } from "../../types/TypeUser";
+
+const LoginFormSchema = z.object({
+  email: z.string().email("Por favor entre com um email valido"),
+  password: z.string().min(6, "Digite uma senha valida"),
+});
 
 const Login = () => {
-  return <S.Container></S.Container>;
+  const form = useForm({
+    schema: LoginFormSchema,
+  });
+
+  async function handleLogin(values: LoginProps) {
+    console.log(values);
+  }
+
+  return (
+    <S.Container>
+      <S.BannerCampany>
+        <img src={imageCompany} alt="" />
+      </S.BannerCampany>
+      <S.ContentFormField>
+        <header>
+          <img src={logo} alt="logo" />
+          <h3>Faça login e comece a aprender!</h3>
+        </header>
+
+        <Form
+          form={form}
+          onSubmit={(values) => handleLogin(values)}
+          id="formField"
+        >
+          <Input label="Email" id="InputEmail" {...form.register("email")} />
+
+          <Input
+            label="Senha"
+            id="InputPass"
+            type="password"
+            {...form.register("password")}
+          />
+
+          <div id="forgot">
+            <Link to={"/recoverpass"}>
+              <span>Esqueceu a senha?</span>
+              <ArrowRight />
+            </Link>
+          </div>
+
+          <Button.Action type="submit">
+            <Button.Title title="Entrar" />
+          </Button.Action>
+
+          <div id="signup">
+            <span>Não tem uma conta ainda?</span>
+            <Link to={"/signup"}>
+              <span>Inscrever-se</span>
+            </Link>
+          </div>
+        </Form>
+      </S.ContentFormField>
+    </S.Container>
+  );
 };
 
 export default Login;
--- src/pages/Login/styles.ts
+++ src/pages/Login/styles.ts
@@ -4,4 +4,121 @@ export const Container = styled.div`
   width: 100%;
   min-height: 100vh;
   background-color: ${(props) => props.theme.colors.black};
+  display: flex;
+
+  header {
+    text-align: center;
+    margin-bottom: 26px;
+
+    img {
+      margin: 24px 0;
+      width: 92px;
+    }
+
+    h3 {
+      font-size: 1rem;
+      font-family: "PoppinsLight";
+    }
+  }
+
+  #formField {
+    display: flex;
+    flex-direction: column;
+  }
+
+  #forgot {
+    margin: 32px 0;
+    display: flex;
+    justify-content: flex-end;
+
+    a {
+      display: flex;
+      align-items: center;
+      gap: 5px;
+      text-decoration: none;
+
+      span {
+        color: ${(props) => props.theme.colors.black[200]};
+      }
+      svg {
+        path {
+          fill: ${(props) => props.theme.colors.pink[200]};
+        }
+      }
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  #signup {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+    margin-top: 32px;
+
+    a {
+      margin-top: 12px;
+      text-decoration: none;
+
+      span {
+        color: ${(props) => props.theme.colors.pink[200]};
+      }
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+
+  @media (${(props) => props.theme.device.tablet}) {
+    display: block;
+    flex-direction: column;
+    padding: 16px;
+    position: relative;
+  }
+
+  @media (${(props) => props.theme.device.mobileL}) {
+    align-items: center;
+    justify-content: center;
+    padding: 16px 0;
+  }
+`;
+
+export const BannerCampany = styled.section`
+  min-height: 100vh;
+  position: relative;
+
+  img {
+    max-height: 100vh;
+    top: 0;
+    position: sticky;
+  }
+
+  @media (${(props) => props.theme.device.tablet}) {
+    display: none;
+  }
+`;
+
+export const ContentFormField = styled.main`
+  width: 100%;
+  padding: 24px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+
+  form {
+    width: 100%;
+    max-width: 500px;
+  }
+
+  @media (${(props) => props.theme.device.laptop}) {
+    padding: 32px;
+  }
+
+  @media (${(props) => props.theme.device.tablet}) {
+    padding: 24px;
+  }
 `;
