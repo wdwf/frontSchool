5e0c6eb5577e4d4cf5f576c817d4eef6681a0259
wdwf <w_dwf@hotmail.com>
2023-08-19 15:40:37 -0300

    :construction: chore: add new rotes user

--- src/router/NonAuthenticatedRoutes.tsx
+++ src/router/NonAuthenticatedRoutes.tsx
@@ -6,14 +6,20 @@ import {
 } from "react-router-dom";
 
 import Login from "../pages/Login";
+import Subscribe from "../pages/Subscribe";
+import RecoverPass from "../pages/RecoverPass";
+import LandingPage from "../pages/LandingPage";
 
 export default function NonAuthenticatedRoutes() {
   return (
     <Router>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Subscribe />} />
+        <Route path="/recoverpass" element={<RecoverPass />} />
+        <Route path="/" element={<LandingPage />} />
 
-        <Route path="/" element={<Login />} />
+        <Route path="/" element={<LandingPage />} />
         <Route path="*" element={<Navigate to={"/landpage"} replace />} />
       </Routes>
     </Router>
